8261d6f076921c31a20722fcb9a6cb0e5e671929
Hanna Moise <hannamoise@gmail.com>
2024-11-08 22:55:53 +0200

    lab5_ex1-12

---
+++ Pages/Members/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Moise_Hanna_Lab2.Data;
+using Moise_Hanna_Lab2.Models;
+
+namespace Moise_Hanna_Lab2.Pages.Members
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Moise_Hanna_Lab2.Data.Moise_Hanna_Lab2Context _context;
+
+        public DetailsModel(Moise_Hanna_Lab2.Data.Moise_Hanna_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Member Member { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member.FirstOrDefaultAsync(m => m.ID == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Member = member;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Members/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Moise_Hanna_Lab2.Data;
+using Moise_Hanna_Lab2.Models;
+
+namespace Moise_Hanna_Lab2.Pages.Members
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Moise_Hanna_Lab2.Data.Moise_Hanna_Lab2Context _context;
+
+        public IndexModel(Moise_Hanna_Lab2.Data.Moise_Hanna_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Member> Member { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Member = await _context.Member.ToListAsync();
+        }
+    }
+}
