a36e09c7550f34c15fe6e7fbeaf9b877670874ef
Hanna Moise <hannamoise@gmail.com>
2024-11-20 19:32:26 +0200

    lab5

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Moise_Hanna_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Moise_Hanna_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Moise_Hanna_Lab2Context' not found.")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LibraryIdentityContext>();
+
 builder.Services.AddDbContext<LibraryIdentityContext>(options =>
 
 options.UseSqlServer(builder.Configuration.GetConnectionString("Moise_Hanna_Lab2Context") ?? throw new InvalidOperationException("Connection string'Moise_Hanna_Lab2Context' not found.")));
