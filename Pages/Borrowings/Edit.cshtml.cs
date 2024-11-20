2053f3ff39d152b6fd8db9f71794e8445f87c187
Hanna Moise <hannamoise@gmail.com>
2024-11-20 20:00:31 +0200

    lab6

--- Pages/Borrowings/Edit.cshtml.cs
+++ Pages/Borrowings/Edit.cshtml.cs
@@ -36,8 +36,8 @@ namespace Moise_Hanna_Lab2.Pages.Borrowings
                 return NotFound();
             }
             Borrowing = borrowing;
-           ViewData["BookID"] = new SelectList(_context.Book, "Title", "Title");
-           ViewData["MemberID"] = new SelectList(_context.Member, "FullName", "FullName");
+           ViewData["BookID"] = new SelectList(_context.Book, "ID", "Title");
+           ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName");
             return Page();
         }
 
