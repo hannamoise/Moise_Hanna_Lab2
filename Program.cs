90abce7d0ad02912e520bf039b5758a37adf56f7
Hanna Moise <hannamoise@gmail.com>
2024-11-20 20:03:12 +0200

    laborator62.0

--- Program.cs
+++ Program.cs
@@ -30,6 +30,8 @@ builder.Services.AddRazorPages(options =>
     options.Conventions.AllowAnonymousToPage("/Books/Index");
     options.Conventions.AllowAnonymousToPage("/Books/Details");
     options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Publishers", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Categories", "AdminPolicy");
 });
 var app = builder.Build();
 
